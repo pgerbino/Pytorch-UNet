2d66388e37d29f55f59fb6da6c42f00a92446d8d
piero gerbino <piero_gerbino@hotmail.com>
2024-07-07 14:10:09 +0100

    refactor: Add decoder part to U-Net architecture

--- code/unet/unet_model.py
+++ code/unet/unet_model.py
@@ -3,6 +3,45 @@
 from .unet_parts import *  # Importing necessary parts from the unet_parts module
 
 
+# why does uunet work please see the following link
+# https://towardsdatascience.com/understanding-semantic-segmentation-with-unet-6be4f42d4b47
+# the U-Net architecture is composed of two parts: the encoder and the decoder
+# the encoder is responsible for capturing the low level features of the image
+# the decoder is responsible for upsampling the image back to its original resolution
+# while reducing the number of channels
+# the U-Net architecture is composed of a series of convolutional blocks
+# each convolutional block is composed of two convolutional layers followed by a ReLU activation function
+# the first convolutional layer has a kernel size of 3x3 and a padding of 1
+# the second convolutional layer has a kernel size of 3x3 and a padding of 1
+# the convolutional layers are followed by a ReLU activation function
+# the output of the ReLU activation function is passed to the next convolutional block
+# the U-Net architecture is composed of a series of downsample blocks
+# each downsample block is composed of a max pooling layer followed by a convolutional block
+# the max pooling layer has a kernel size of 2x2 and a stride of 2
+# the convolutional block is composed of two convolutional layers followed by a ReLU activation function
+# the output of the ReLU activation function is passed to the next downsample block
+# the U-Net architecture is composed of a series of upsample blocks
+# each upsample block is composed of an upsampling layer followed by a convolutional block
+# the upsampling layer has a kernel size of 2x2 and a stride of 2
+# the convolutional block is composed of two convolutional layers followed by a ReLU activation function
+# the output of the ReLU activation function is passed to the next upsample block
+# the U-Net architecture is composed of an output convolutional block
+# the output convolutional block is composed of a convolutional layer followed by a ReLU activation function
+# the output of the ReLU activation function is passed to the next convolutional layer
+# the output of the last convolutional layer is the output of the U-Net architecture
+# the output of the U-Net architecture is a segmentation mask
+# the segmentation mask is a binary image that indicates the presence or absence of an object in the image
+# the U-Net architecture is trained using a loss function called the dice loss
+# the dice loss is a measure of the overlap between the predicted segmentation mask and the ground truth segmentation mask
+# the dice loss is defined as the intersection over the union of the predicted segmentation mask and the ground truth segmentation mask
+# the dice loss is minimized during training by adjusting the weights of the U-Net architecture
+# the U-Net architecture is trained using a dataset of images and their corresponding segmentation masks
+# the U-Net architecture is trained using a technique called backpropagation
+
+# for a deeper mathemtical treatment of uunet please see the following link
+# https://arxiv.org/abs/1505.04597
+
+
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
@@ -16,6 +55,17 @@ class UNet(nn.Module):
         self.down3 = (Down(256, 512))  # Downsample block 3
         factor = 2 if bilinear else 1
         self.down4 = (Down(512, 1024 // factor))  # Downsample block 4
+        # at this point we have the original image resolution reduced by a factor of 16
+        # and the number of channels increased to 1024
+        # we can now start the upsampling process
+        # what this represents is the decoder part of the U-Net architecture
+        # the decoder part is responsible for upsampling the image back to its original resolution
+        # while reducing the number of channels
+        # so in a sense we have a low level representation of the original image 1024 times
+        # and we want to upsample this representation to the original resolution
+        # while reducing the number of channels to the number of classes
+        # the number of classes is the number of output classes
+        # in the case of the dataset we are using, the number of classes is 1
         self.up1 = (Up(1024, 512 // factor, bilinear))  # Upsample block 1
         self.up2 = (Up(512, 256 // factor, bilinear))  # Upsample block 2
         self.up3 = (Up(256, 128 // factor, bilinear))  # Upsample block 3
--- code/unet/unet_parts.py
+++ code/unet/unet_parts.py
@@ -5,24 +5,71 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# you may wonder why we need double convolution and not just one convolutional layer. The reason is that the double convolutional layer allows the network to learn more complex patterns in the input data.
+# The first convolutional layer extracts low-level features from the input image, such as edges and corners. The second convolutional layer extracts higher-level features, such as shapes and textures.
+# By using two consecutive convolutional layers, the network can learn more complex patterns in the input data and make better predictions.
+# The double convolutional layer is a common building block in many modern neural networks because it is simple and effective at learning complex patterns in the input data.
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
     def __init__(self, in_channels, out_channels, mid_channels=None):
+        """
+        DoubleConv module consists of two consecutive convolutional layers with batch normalization and ReLU activation.
+        
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            mid_channels (int, optional): Number of intermediate channels. If not provided, it is set to out_channels.
+        """
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
+            #conv2d can be explained to an undergraduate as a function that takes an image as input and outputs another image.
+            #The convolutional layer is the building block of a CNN. It is a mathematical operation that takes an input image, applies a filter (or kernel) to it, and produces an output image.
+            #The filter is a small matrix that slides over the input image, performing element-wise multiplication with the input values and summing the results to produce a single output value.
+            #The filter is then moved to the next position, and the process is repeated until the entire image has been processed.
+            #The output image is smaller than the input image because the filter cannot be applied to the edges of the input image.
+            #The size of the output image is determined by the size of the filter and the stride, which is the number of pixels the filter moves each time it is applied.
+            #The convolutional layer is used to extract features from the input image. It is typically followed by a pooling layer, which reduces the size of the output image by downsampling it.
+            #The pooling layer is used to reduce the size of the output image and make the network more computationally efficient. It does this by taking a small region of the input image and computing a single output value for that region.
+            #The pooling layer is typically followed by another convolutional layer, which extracts more features from the downsampled image. This process is repeated multiple times to create a deep network that can learn complex patterns in the input data.
+            
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),  # 2D convolutional layer
+            ##batchnorm2d can be explained to an undergraduate as a function that normalizes the input to a neural network by adjusting and scaling the activations.
+            #It is used to make the training process more stable and faster by reducing the internal covariate shift, which is the change in the distribution of the input data to a layer of the network.
+            #Batch normalization is typically applied after the activation function in a neural network. It takes the output of the activation function and normalizes it by subtracting the mean and dividing by the standard deviation of the input data.
+            #This helps to ensure that the input data to the next layer of the network is in a consistent range, which can improve the performance of the network.
+            #Batch normalization is used in many modern neural networks because it can help to prevent overfitting and improve the generalization of the model.
+
             nn.BatchNorm2d(mid_channels),  # Batch normalization layer
+            #relu can be explained to an undergraduate as a function that introduces non-linearity into a neural network.
+            #It is used to add non-linearities to the network, which allows it to learn complex patterns in the input data.
+            #The ReLU function is a simple non-linear activation function that takes the input value and returns the maximum of that value and zero.
+            #This means that if the input value is positive, the output will be the same as the input value. If the input value is negative, the output will be zero.
+            #The ReLU function is used in many modern neural networks because it is simple and computationally efficient, and it has been shown to work well in practice.
+            #It is typically applied after the convolutional and pooling layers in a neural network to introduce non-linearity into the network and allow it to learn complex patterns in the input data.
+        
             nn.ReLU(inplace=True),  # ReLU activation function
+            #kernel size means the size of the filter that is applied to the input image. The kernel size is typically a square matrix with an odd number of rows and columns, such as 3x3 or 5x5.
+            #The kernel size determines the size of the region of the input image that is processed by the filter. A larger kernel size will capture more information from the input image, but it will also increase the computational cost of the operation.
+            #The padding parameter is used to add zeros around the edges of the input image before applying the filter. This is done to ensure that the output image has the same size as the input image.
+            #The padding parameter is typically set to half the kernel size, so that the filter is applied to the center of the input image.
+            #The bias parameter is used to add a constant value to the output of the convolutional operation. It is typically set to False, because the batch normalization layer already includes a bias term.
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),  # 2D convolutional layer
             nn.BatchNorm2d(out_channels),  # Batch normalization layer
             nn.ReLU(inplace=True)  # ReLU activation function
         )
 
     def forward(self, x):
+        """
+        Forward pass of the DoubleConv module.
+        
+        Args:
+            x (torch.Tensor): Input tensor.
+        
+        Returns:
+            torch.Tensor: Output tensor after passing through the double convolutional layers.
+        """
         return self.double_conv(x)
 
 
@@ -32,6 +79,11 @@ class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
+            #maxpool2d can be explained to an undergraduate as a function that reduces the size of an input image by taking the maximum value in a small region of the image.
+            #It is used to downsample the input image and make the network more computationally efficient. It does this by taking a small region of the input image and computing the maximum value for that region.
+            #The max pooling layer is typically applied after the convolutional layer in a neural network. It is used to reduce the size of the output image and make the network more computationally efficient.
+            #The max pooling layer is typically followed by another convolutional layer, which extracts more features from the downsampled image. This process is repeated multiple times to create a deep network that can learn complex patterns in the input data.
+
             nn.MaxPool2d(2),  # Max pooling layer
             DoubleConv(in_channels, out_channels)  # DoubleConv layer
         )
@@ -48,7 +100,15 @@ class Up(nn.Module):
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
+            #upsample can be explained to an undergraduate as a function that increases the size of an input image by interpolating the pixel values.
+            #It is used to upsample the input image and make the network more computationally efficient. It does this by increasing the size of the input image by a factor of two.
+            #The upsample layer is typically used in the decoder part of a neural network to increase the size of the feature maps before concatenating them with the feature maps from the encoder part.
+            #The upsample layer is typically followed by a convolutional layer, which extracts more features from the upsampled image. This process is repeated multiple times to create a deep network that can learn complex patterns in the input data.
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)  # Upsampling layer
+            #even though we have already upsampled we use doubleconv to decode the features
+            #The reason for this is that the upsample layer only increases the size of the input image, but it does not extract any new features from the image.
+            #The convolutional layer is used to extract features from the input image. It takes the upsampled image as input and applies a filter to it to extract features such as edges, corners, and shapes.
+            #The convolutional layer is typically followed by a batch normalization layer, which normalizes the input to the layer and makes the training process more stable and faster.            
             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)  # DoubleConv layer
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)  # Transposed convolutional layer
